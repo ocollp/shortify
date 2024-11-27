518d8ba16d2bce23a9a66d060d7a64719dfcfcc5
Olga Coll <olgacoll94@gmail.com>
2024-11-27 12:20:10 +0100

    feat: update url db table with the new field

--- src/main/resources/db/changes/v1/000-create-table-url.sql
+++ src/main/resources/db/changes/v1/000-create-table-url.sql
@@ -1,6 +1,7 @@
 CREATE TABLE public.url (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
     original_url VARCHAR(255) NOT NULL,
+    shortened_path VARCHAR(255) NOT NULL,
     shortened_url VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
